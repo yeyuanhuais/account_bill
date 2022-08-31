358aa0af7847ed77a8ff7ab6cfb46bf86fc816a8
yeyuanhua <1378476425@qq.com>
2022-08-31 17:08:42 +0800

    波浪球修改

--- wechat_client/pages/bill/bill.js
+++ wechat_client/pages/bill/bill.js
@@ -1,66 +1,94 @@
 // pages/bill/bill.js
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    billData: [
+      {
+        time: "2022",
+        type: "year",
+        children: [
+          {
+            time: "8",
+            type: "month",
+            income: "345345",
+            expenditure: "3233",
+            children: [
+              { time: "30", type: "day", income: "345345", expenditure: "3233" },
+              { time: "29", type: "day", income: "345345", expenditure: "3233" },
+              { time: "28", type: "day", income: "345345", expenditure: "3233" },
+              { time: "27", type: "day", income: "345345", expenditure: "3233" },
+              { time: "27", type: "day", income: "345345", expenditure: "3233" },
+            ],
+          },
+          {
+            time: "7",
+            type: "month",
+            income: "345345",
+            expenditure: "3233",
+            children: [
+              {
+                time: "30",
+                type: "day",
+                income: "345345",
+                expenditure: "3233",
+                children: [
+                  { icon: "30", name: "交通", type: "day", income: "345345" },
+                  { icon: "29", name: "交通", type: "day", expenditure: "3233" },
+                  { icon: "28", name: "交通", type: "day", income: "345345" },
+                  { icon: "27", name: "交通", type: "day", income: "345345" },
+                  { icon: "27", name: "交通", type: "day", expenditure: "3233" },
+                ],
+              },
+              { time: "29", type: "day", income: "345345", expenditure: "3233" },
+              { time: "28", type: "day", income: "345345", expenditure: "3233" },
+              { time: "27", type: "day", income: "345345", expenditure: "3233" },
+              { time: "27", type: "day", income: "345345", expenditure: "3233" },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {},
 
-    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
 
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    }
-})
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
+});
